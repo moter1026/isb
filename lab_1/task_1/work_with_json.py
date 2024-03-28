7270eb8364c9c9b8e277828c072dd090e10173bf
Matvey <matvej.sidoroff2016@yandex.ru>
2024-03-28 19:19:29 +0400

    add logging, try: exept: and rename functions

--- lab_1/task_1/work_with_json.py
+++ lab_1/task_1/work_with_json.py
@@ -1,6 +1,6 @@
 import json
 
-def readJSONFile(json_file_name) -> dict:
+def read_json_file(json_file_name) -> dict:
     data = {}
     with open(json_file_name, "r", encoding="utf-8") as JSON_file:
         data = json.load(JSON_file)
