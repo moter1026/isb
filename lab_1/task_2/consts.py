6de833a9824104332cae9fea1e5112bc9dd451f7
moter1026 <matvej.sidoroff2016@yandex.ru>
2024-03-28 20:26:02 +0400

    add logging, try: exept and rename functions

--- lab_1/task_2/consts.py
+++ lab_1/task_2/consts.py
@@ -1,10 +1,14 @@
 import work_with_json
 
 
-COLOR_BLUE = '\033[94m'
-COLOR_YELLOW = '\033[93m'
 COLOR_RED = '\033[91m'
+COLOR_GREEN = '\033[92m'
+COLOR_YELLOW = '\033[93m'
+COLOR_BLUE = '\033[94m'
+COLOR_MAGENTA = '\033[95m'
+COLOR_CYAN = '\033[96m'
 COLOR_RESET = '\033[0m'  # Сброс цвета
 
 
-FILES = work_with_json.readJSONFile("./files.json")
+
+FILES = work_with_json.read_json_file("./files.json")
